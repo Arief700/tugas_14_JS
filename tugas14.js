7d1b6ec0d647af99f0d526ba5995937b28828873
Arief Setiawan <Aremor14@gmail.com>
2020-05-12 00:11:28 +0700

    Tugas 14 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas14.js
@@ -0,0 +1,8 @@
+var number = 37;
+var myDate = 14;
+var result = number / myDate;
+console.log("hasil operasi pembagian :", result);
+
+console.log("Pembulatan Round :", Math.round(result));
+console.log("Pembulatan Ceil :", Math.ceil(result));
+console.log("Pembulatan floor :", Math.floor(result));
